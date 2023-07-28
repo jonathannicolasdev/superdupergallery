96ebcf20ec9c178dac3a05a3db1939f19d71b21c
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-28 18:49:29 +0700

    feat: revamp dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react"
 
 import { authenticator } from "~/services/auth.server"
 import { useRootLoaderData } from "~/hooks"
-import { Button, Layout, UserCard } from "~/components"
+import { Button, Layout } from "~/components"
 
 export const loader = async ({ request }: LoaderArgs) => {
   await authenticator.isAuthenticated(request, { failureRedirect: "/login" })
@@ -18,36 +18,20 @@ export default function DashboardRoute() {
   }
 
   return (
-    <Layout className="space-y-8 p-4 sm:px-8">
+    <Layout
+      hasHeader={false}
+      hasFooter={false}
+      className="space-y-8 p-4 sm:px-8"
+    >
       <header>
-        <span>Welcome,</span>
-        <Link to="/dashboard">
-          <h1 className="hover-opacity text-brand">{userData?.name}</h1>
-        </Link>
-        <p className="text-muted-foreground">This is your dashboard.</p>
-      </header>
-
-      <section className="space-y-2">
-        <h4>Your profile card:</h4>
-        <Link to={`/${userData.username}`} className="block">
-          <UserCard user={userData as any} />
-        </Link>
-      </section>
-
-      <section className="flex flex-wrap gap-2">
-        <Button asChild size="sm" variant="secondary">
-          <Link to="/settings">Settings</Link>
-        </Button>
-        <Button asChild size="sm" variant="secondary">
-          <Link to="/settings/profile">Edit Profile</Link>
-        </Button>
-        <Button asChild size="sm" variant="outline">
-          <Link to="/uploadcare">Uploadcare</Link>
-        </Button>
+        <p className="text-muted-foreground">
+          This is the Administration dashboard. You're logged in as{" "}
+          <b>{userData?.name}</b>
+        </p>
         <Button asChild size="sm" type="submit" variant="destructive">
           <Link to="/logout">Logout</Link>
         </Button>
-      </section>
+      </header>
     </Layout>
   )
 }
