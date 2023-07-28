e414673ba7598ebfa9aea1e1045bfaa9da48f31e
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-28 19:25:52 +0700

    feat: more data artworks

--- app/data/artworks.ts
+++ app/data/artworks.ts
@@ -1,14 +1,142 @@
-import type { Artist, Artwork } from "@prisma/client"
+import type { Artwork } from "@prisma/client"
 
-export type DataArtwork = Pick<Artwork, "title" | "medium" | "size"> & {
-  exhibitionName?: string
-  artistName?: Artist["name"]
-  year?: Artwork["year"]
-  price?: Artwork["price"]
-  imageURL?: string
-}
+export type DataArtwork = Pick<Artwork, "title" | "medium" | "size"> &
+  Partial<Artwork> & {
+    isSold?: boolean
+    artistName?: string
+    exhibitionName?: string
+    imageURL?: string
+  }
 
-export const dataArtworks: DataArtwork[] = [
+export const dataArtworksName: DataArtwork[] = [
+  {
+    exhibitionName: "",
+    artistName: "",
+    title: "",
+    medium: "",
+    size: "",
+    year: 2021,
+    price: 100,
+    imageURL: "",
+  },
+]
+
+export const dataArtworksLikeCandy: DataArtwork[] = [
+  {
+    exhibitionName: "Like Candy",
+    artistName: "Viktor Tejada",
+    title: "Weekend Girl",
+    medium: "Acrylic On Canvas",
+    size: "36x26 Inches",
+    year: 2021,
+    price: 20000,
+    imageURL:
+      "https://ucarecdn.com/bf0d1141-a35b-4358-8532-68d567d5aff1/-/preview/938x432/-/quality/smart/-/format/auto/",
+  },
+  {
+    exhibitionName: "Like Candy",
+    artistName: "Viktor Tejada",
+    title: "6000 Freedoms Per Minute",
+    medium: "Oil On Canvas",
+    size: "24x 18 Inches",
+    year: 2021,
+    price: 15000,
+    imageURL:
+      "https://ucarecdn.com/16f67447-45ef-4e78-bbe6-76c51716bc80/-/preview/938x432/-/quality/smart/-/format/auto/",
+  },
+  {
+    artistName: "Aldrin Tamildes",
+    title: "Corrupted",
+    medium: "Acrylic On Canvas",
+    size: `36" x 36" `,
+    price: 40000,
+    imageURL: "https://media.graphassets.com/buSCdJDcTpSHszovGd1T",
+  },
+  {
+    artistName: "Tokwa Penaflorida",
+    title: "Pearl Necklace",
+    medium: "Acrylic on canvas",
+    size: `12" x 12"`,
+    price: 0,
+    imageURL: "https://media.graphassets.com/xSDBUZfwTpeLQSf72qUA",
+  },
+  {
+    artistName: "Anton Bolardo",
+    title: "Forgive a girl for being lonely",
+    medium: "Acrylic on canvas",
+    size: `24" x 36"`,
+    price: 0,
+    isSold: true,
+    imageURL: "https://media.graphassets.com/DMWgIi0RXSX24GvuKfZh",
+  },
+  {
+    artistName: "Gabriel Colico",
+    title: "Enjoy Life Today",
+    medium: "Acrylic On Canvas",
+    size: `24" x 36"`,
+    price: 35998,
+    imageURL: "https://media.graphassets.com/AcGRIJamTqCZYyvBpYp4",
+  },
+  {
+    artistName: "Gabriel Colico",
+    title: "Be Grateful For Everything",
+    medium: "Acrylic On Canvas",
+    size: `24" x 36"`,
+    price: 36000,
+    imageURL: "https://media.graphassets.com/WulRjqxhQlKWz05WB5sc",
+  },
+  {
+    artistName: "Tisha Paculba",
+    title: "Children Of The Forest First Series - Healer",
+    medium: "Oil On Canvas",
+    size: `24" x 18"`,
+    price: 30000,
+    imageURL: "https://media.graphassets.com/4mLvGFIYS3W0Fb7iR9s9",
+  },
+  {
+    artistName: "Tisha Paculba",
+    title: "Children Of The Forest First Series - Dancer",
+    medium: "Oil On Canvas",
+    size: `12" x 16"`,
+    price: 20000,
+    imageURL: "https://media.graphassets.com/lMWE6BLhQyaxoqkaL7dG",
+  },
+  {
+    artistName: "Tisha Paculba",
+    title:
+      "Children Of The Forest First Series - Himalayan White-Browed Rosefinch",
+    medium: "Oil On canvas",
+    size: `9" x 11" `,
+    price: 15000,
+    imageURL: "https://media.graphassets.com/q4Or5AiRSXO5e9mB5vdr",
+  },
+  {
+    artistName: "Bea Aben",
+    title: "Stage R 1",
+    medium: "Mixed Media on Water Color Paper",
+    size: `14" x 18"`,
+    price: 18000,
+    imageURL: "https://media.graphassets.com/GvnGUSlNTcGgeRctLNEV",
+  },
+  {
+    artistName: "Bea Aben",
+    title: "Stage R 2",
+    medium: "Mixed Media On Watercolor Paper",
+    size: `14" x 18"`,
+    price: 18000,
+    imageURL: "https://media.graphassets.com/IlQi4jwMSeu36JB5SjVC",
+  },
+  {
+    artistName: "Bea Aben",
+    title: "Dream Surfer # 22:22",
+    medium: "Mixed Media On Watercolor Paper",
+    size: `31.5" x 23.5"`,
+    price: 44000,
+    imageURL: "https://media.graphassets.com/Y8Hv48n3RX25bNDDcjme",
+  },
+]
+
+export const dataArtworksJourney: DataArtwork[] = [
   {
     exhibitionName: "Journey",
     artistName: "Sarcita Yasa",
@@ -75,6 +203,9 @@ export const dataArtworks: DataArtwork[] = [
     imageURL:
       "https://ucarecdn.com/1e6708cc-e0f7-43ae-8277-651e81f4d51b/-/preview/938x432/-/quality/smart/-/format/auto/",
   },
+]
+
+export const dataArtworksWound: DataArtwork[] = [
   {
     exhibitionName: "Wound",
     artistName: "Laura Abejo",
@@ -108,4 +239,10 @@ export const dataArtworks: DataArtwork[] = [
     imageURL:
       "https://ucarecdn.com/7e266377-52d5-45d3-be88-f960e0560524/-/preview/938x432/-/quality/smart/-/format/auto/",
   },
+]
+
+export const dataArtworks = [
+  ...dataArtworksLikeCandy, // 1
+  ...dataArtworksJourney, // 44
+  ...dataArtworksWound, // 35
 ]
--- app/data/exhibitions.ts
+++ app/data/exhibitions.ts
@@ -1,33 +1,41 @@
 import type { Exhibition } from "@prisma/client"
 
-export type DataExhibition = Pick<Exhibition, "title"> & {
+export type DataExhibition = Pick<Exhibition, "edition" | "title"> & {
   date?: Exhibition["date"]
   imageURL?: string
 }
 
 export const dataExhibitions: DataExhibition[] = [
   {
-    // edition: 6,
-    title: "Sixth",
-    date: new Date("2021-01-01"),
-    imageURL: "",
+    edition: 1,
+    title: "Like Candy",
+    date: new Date("2021-10-10"),
+    imageURL:
+      "https://ucarecdn.com/121e8b2a-01a1-491f-b0a9-af9d940086b9/-/preview/938x432/-/quality/smart/-/format/auto/",
   },
   {
-    // edition: 43,
+    edition: 2,
+    title: "The Universe Smiles Back at You",
+    date: new Date("2021-12-05"),
+    imageURL:
+      "https://ucarecdn.com/edaa89d9-539e-41e8-bb03-3de34229ab2a/-/preview/938x432/-/quality/smart/-/format/auto/",
+  },
+  {
+    edition: 43,
     title: "Divine Foolishness",
     date: new Date("2023-06-11"),
     imageURL:
       "https://ucarecdn.com/1e48a0fd-881f-4537-a076-0c37deb9ceda/-/preview/938x432/-/quality/smart/-/format/auto/",
   },
   {
-    // edition: 44,
+    edition: 44,
     title: "Journey",
     date: new Date("2023-06-11"),
     imageURL:
       "https://ucarecdn.com/b352054e-065c-46c8-9142-53030bb18cb1/-/preview/938x432/-/quality/smart/-/format/auto/",
   },
   {
-    // edition: 45,
+    edition: 45,
     title: "Wound",
     date: new Date("2023-07-15"),
     imageURL:
